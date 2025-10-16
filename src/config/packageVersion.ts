560bbfd5b10d344b5f51b3c4a10724c00adedb77
David Kerber <dave@kerber.io>
2025-10-15 20:23:39 -0500

    feat: Use static iam-collect version instead of reading package.json

--- src/config/packageVersion.ts
+++ src/config/packageVersion.ts
@@ -1,28 +1,8 @@
-import { getPackageFileReader } from '../utils/readPackageFile.js'
-
-interface PackageInfo {
-  version: string
-}
-
-let packageCache: PackageInfo | undefined = undefined
-
-/**
- * Get the package data version
- *
- * @returns the package data version
- */
-async function getPackageData(): Promise<PackageInfo> {
-  if (!packageCache) {
-    const pkgData = await getPackageFileReader().readFile(['package.json'])
-    packageCache = JSON.parse(pkgData)
-  }
-  return packageCache!
-}
+import { IAM_COLLECT_VERSION } from './version.js'
 
 /**
  * Get the version of the package
  */
 export async function iamCollectVersion(): Promise<string> {
-  const data = await getPackageData()
-  return data.version
+  return IAM_COLLECT_VERSION
 }
